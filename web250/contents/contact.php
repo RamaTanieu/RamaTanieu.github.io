93b18d170d22774e24c9dd30dedd49ed167b1139
RamaTanieu <156043075+RamaTanieu@users.noreply.github.com>
2024-05-04 15:34:11 -0400

    contact.php

---
+++ web250/contents/contact.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Rama Tanieu | WEB250 | Contract</title>
+	<link rel = "Stylesheet" type = "text/css" href = "Styles/default.css">
+	<script src="scripts/addHeaderAndFooter.js" defer></script>
+</head>
+
+<body>
+	<main>
+		<header>
+		<h1>Rama Tanieu | Avocet  | WEB250|</h1>
+		<nav>
+			<a href="index.html">Home</a> |
+			<a href="introduction.html">Introduction</a> |
+			<a href="contract.html">Contract</a> |
+			<a href="brand.html">Brand</a>
+		</nav>
+		</header>
+		<main>
+<br>
+<br>
+		<h2>Course Contract</h2>
+
+
+<br>
+<br>
+		<p>
+			I, <a href="introduction.html">Rama E Tanieu</a>, agree to abide by the terms in my Spring 2024 WEB250 Database Driven Websites Course with my instructor, D.I. von Briesen.
+		</p>
+		
+		<p>
+			I understand that all work that I do on my school and personal websites will be publicly available to the world. 
+			I will not put information there that is inappropriate for schoolwork, or that I wish to keep private.
+		</p>
+		
+		<p>
+			I also understand that it is my work that counts for attendance, not logins or showing up for class. 
+			As such, failure to turn in assignments may show as absences.
+		</p>
+		
+
+		<p>
+			Signed: <em>Rama E Tanieu, 22 February 2024</em>
+		</p>
+	</main>
+
+		<footer> 
+		<nav>
+			<a href="https://github.com/RamaTanieu">GitHub</a> |
+			<a href="https://ramatanieuj.github.io">GitHub.io</a> |
+			<a href="https://ramatanieu.github.io/web250.io/">WEB250.io</a> |
+			<a href="https://www.codecademy.com/profiles/ramatanieu">Codecademy</a> |
+			<a href="https://www.freecodecamp.org/ramatanieu">FreeCodeCamp</a> |
+			<a href="https://jsfiddle.net/user/ramatanieu/fiddles/">JSFiddle</a> 
+			
+		</nav>
+		<a href="https://validator.w3.org/check?uri=https://RamaTanieu.github.io/web250.io/introduction.html">
+		  <img src="Images/html_validation_button.gif" alt="Validate HTML">
+		</a>
+		 
+		<a href="http://jigsaw.w3.org/css-validator/validator?uri=https://RamaTanieu.github.io/web250.io/introduction.html">
+		 <img src="Images/validation_button_css_rectangle_blue.gif" alt="Validate CSS">
+		</a>
+		</footer>
+	</body>
+</html>
