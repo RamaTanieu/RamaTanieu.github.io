99c60f5039215f7b46e82813189c442eba56598e
RamaTanieu <156043075+RamaTanieu@users.noreply.github.com>
2024-05-04 15:59:00 -0400

    brand.php

--- web250/contents/brand.php
+++ web250/contents/brand.php
@@ -1,28 +1,29 @@
 
-<h2 class="alignCenter">Brackett's PC Builders</h2>
-<h3 class="alignCenter">To build the pc you need</h3>
+<h2 class="alignCenter">Brand</h2>
+<h3 class="alignCenter">Ramset Cosmeticd</h3>
 
-<h3 class="alignLeftMain">"Practical PCs for practical needs"</h3>
-<p>Here at BPCB we strive to provide you the customer with the pc build that you need for the purpose that you require.</p>
-<h3 class="alignCenter">Design Elements</h3>
-<ul>
-	<li>defaults</li>
-	<ul>
-		<li>background color: #375fab </li>
-		<li>text color: #EAE8FF </li>
-		<li>font: Palantino</li>
-	</ul>
-	<li>header and main </li>
-	<ul>
-		<li>background color: #1B4965</li>
-	</ul>
-	<li>h1</li>
-	<ul>
-		<li>text color: #70dafa</li>
-		<li>font: Georgia</li>
-	</ul>
-	<li>h2 and h3</li>
-	<ul>
-		<li>text color: #74dbd9</li>
-	</ul>
-</ul>
+<p> Your well being is our mission, we are here to help. Brigthening, unifying and nourishing your skin. We have the perfect skin care products that will leave your skin soft, radiant, luminous and confortable.</p>
+			
+			
+			<h4>Color & Fonts</h4>
+			<ul>
+				<li>defaults</li>
+				<ul>
+					<li>background color: #004225</li>
+					<li>text color: #C83F49</li>
+					<li>font: Palantino</li>
+				</ul>
+				<li>header and div </li>
+				<ul>
+					<li>background color: #004225</li>
+				</ul>
+				<li>h1</li>
+				<ul>
+					<li>text color: #C83F49</li>
+					<li>font: Georgia</li>
+				</ul>
+				<li>h2,h3 and h4</li>
+				<ul>
+					<li>text color: #BEE9E8</li>
+				</ul>
+			</ul>
