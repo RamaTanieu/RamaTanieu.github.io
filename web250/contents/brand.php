5772a6c83ee76906bcdd9339419fb9f10845ea9c
RamaTanieu <156043075+RamaTanieu@users.noreply.github.com>
2024-05-04 15:31:09 -0400

    brand.php

---
+++ web250/contents/brand.php
@@ -0,0 +1,28 @@
+
+<h2 class="alignCenter">Brackett's PC Builders</h2>
+<h3 class="alignCenter">To build the pc you need</h3>
+
+<h3 class="alignLeftMain">"Practical PCs for practical needs"</h3>
+<p>Here at BPCB we strive to provide you the customer with the pc build that you need for the purpose that you require.</p>
+<h3 class="alignCenter">Design Elements</h3>
+<ul>
+	<li>defaults</li>
+	<ul>
+		<li>background color: #375fab </li>
+		<li>text color: #EAE8FF </li>
+		<li>font: Palantino</li>
+	</ul>
+	<li>header and main </li>
+	<ul>
+		<li>background color: #1B4965</li>
+	</ul>
+	<li>h1</li>
+	<ul>
+		<li>text color: #70dafa</li>
+		<li>font: Georgia</li>
+	</ul>
+	<li>h2 and h3</li>
+	<ul>
+		<li>text color: #74dbd9</li>
+	</ul>
+</ul>
