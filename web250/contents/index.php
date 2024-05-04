24411ed7cb23c71a245e3105433bc2ea0505ea7d
RamaTanieu <156043075+RamaTanieu@users.noreply.github.com>
2024-05-04 15:25:57 -0400

    index.php

--- web250/contents/index.php
+++ web250/contents/index.php
@@ -1,1 +1,65 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+		<link rel = "stylesheet" type = "text/css" href = "styles/default.css" />
+		<?php
+			$page = 'Home';
+		if (!empty($_GET['page'])) {
+			$page = '';
+			$page .= ucfirst($_GET['page']);
+		}
+		?>
+		<title>Rama Tanieu | Avocet  | WEB250| <?= ' ' . $page ?></title>
+	</head>
+	<body>
+		<header>
+		<h1>Rama Tanieu | Avocet  | WEB250| WEB250</h1>
+		<nav>
+			<a href="index.php?page=home">Home</a> |
+			<a href="index.php?page=introduction">Introduction</a> |
+			<a href="index.php?page=contract">Contract</a> |
+			<a href="index.php?page=brand">Brand</a> |
+			<a href="index.php?page=forms">Forms</a> |
+			<a href="index.php?page=fizbuzz">Fizz Buzz</a>
+		</nav>
 
+		</header>
+	
+	<!-- dynamic content goes here -->
+	<main>
+	<h2><?= $page ?></h2>
+	<?php
+	$url = 'contents/home.php';
+	if (!empty($_GET['contents'])) {
+		$url = 'contents/';
+		$url .= $_GET['contents'] . '/';
+	}
+	if (!empty($_GET['page'])) {
+		$url = 'contents/';
+		$url .= $_GET['page'] . '.php';
+	}
+	include $url;
+	?>
+	</main
+	<footer> 
+	<nav>
+		<a href="https://www.freecodecamp.org/ramatanieu">Freecodecamp</a> |
+			<a href="https://www.codecademy.com/profiles/ramatanieu">Codecadamey</a> |
+			<a href="https://github.com/ramatanieu">Github</a> |
+			<a href="https://jsfiddle.net/user/ramatanieu/fiddles/">JSfiddle</a> |
+			<a href="https://ramatanieu.github.io">Github.io</a>
+	</nav>
+	
+	<figure>
+	<a href="http://validator.w3.org/check?uri=referer">
+	  <img src="images/html_validation_button.gif" alt="Validate HTML">
+	</a>
+	 
+	<a href="http://jigsaw.w3.org/css-validator/validator?uri=https://http://ramatanieu.infinityfreeapp.com/<?=$page?>">
+	 <img src="images/validation_button_css_rectangle_blue.gif" alt="Validate CSS" >
+	</a>
+	</figure>
+	</footer>
+	</body>
+</html>
